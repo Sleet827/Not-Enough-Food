7a11cdbe2727fc5dee9247c6150a5172fc024345
hackcoder1000 <aaravmehra0905@gmail.com>
2021-12-20 15:46:05 +0530

    1.18 Port

--- src/main/java/net/legiblesleet827/nef/setup/WorldSetup.java
+++ src/main/java/net/legiblesleet827/nef/setup/WorldSetup.java
@@ -1,12 +1,11 @@
 package net.legiblesleet827.nef.setup;
 
-import net.legiblesleet827.nef.entity.turkey.Turkey;
-import net.minecraft.world.entity.SpawnPlacements;
-import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 
 public class WorldSetup {
     public static void setup(final BiomeLoadingEvent event) {
-        SpawnPlacements.register(Registration.TURKEY.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.WORLD_SURFACE, Turkey::checkAnimalSpawnRules);
+        event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(Registration.TURKEY.get(), 10, 4, 4));
     }
 }
